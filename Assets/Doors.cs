16c332d8bc414cace1926c37a417b53bee961320
STmihan <mih.dunaev@gmail.com>
2021-07-25 21:41:16 +0300

    Arrow 3

--- Assets/Doors.cs
+++ Assets/Doors.cs
@@ -7,27 +7,25 @@ public class Doors : MonoBehaviour
 {
     [SerializeField] private GameObject Forward;
     private GameObject Arrow;
-    private GameObject Player;
-
-
+    
     private void Start()
     {
         Arrow = GameObject.FindWithTag("Arrow");
-        Player = GameObject.FindWithTag("Player");
     }
 
     void Update()
     {
         var enemies = GameObject.FindGameObjectsWithTag("Enemy");
 
-        if (enemies.Length == 0 || Vector2.Distance(enemies[0].transform.position, Player.transform.position) > 5)
-            Arrow.SetActive(true);
-        else 
-            Arrow.SetActive(false);
-        
         if (enemies.Length == 0)
+        {
             Forward.SetActive(false);
+            Arrow.SetActive(true);
+        }
         else
+        {
             Forward.SetActive(true);
+            Arrow.SetActive(false);
+        }
     }
 }
